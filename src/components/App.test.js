84c01c7f5bc21bd45c0d4897255f0f23b6b3b481
Tyler Hawkins <thawkins@youniqueproducts.com>
2019-01-30 11:53:12 -0700

    minor syntax errors, better formatting

--- src/components/App.test.js
+++ src/components/App.test.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
+import reduxThunk from 'redux-thunk';
 import configureStore from 'redux-mock-store';
 import { shallow } from 'enzyme';
 import { shallowToJson } from 'enzyme-to-json';
@@ -9,44 +10,47 @@ import INITIAL_STATE from '../reducers/initialState';
 
 let wrapper;
 
-beforeEach(() => {
-	wrapper = shallow(<App />);
-});
+describe('App', () => {
+    beforeEach(() => {
+        wrapper = shallow(<App />);
+    });
 
-// Shallow rendering the unconnected component
-it('renders only one app', () => {
-	expect(wrapper.length).toEqual(1);
-});
+    // Shallow rendering the unconnected component
+    it('renders only one app', () => {
+        expect(wrapper.length).toEqual(1);
+    });
 
-// Finding content that the component should render
-it('renders three Button components', () => {
-	expect(wrapper.find('Connect(Button)').length).toEqual(3);
-});
+    // Finding content that the component should render
+    it('renders three Button components', () => {
+        expect(wrapper.find('Connect(Button)').length).toEqual(3);
+    });
 
-// Finding content that the component should render
-it('renders one CounterResults component', () => {
-	expect(wrapper.find('Connect(CounterResults)').length).toEqual(1);
-});
+    // Finding content that the component should render
+    it('renders one CounterResults component', () => {
+        expect(wrapper.find('Connect(CounterResults)').length).toEqual(1);
+    });
 
-// Finding content that the component should render
-it('renders a div with an App-header class', () => {
-	expect(wrapper.find('.App-header').length).toEqual(1);
-});
+    // Finding content that the component should render
+    it('renders a div with an App-header class', () => {
+        expect(wrapper.find('.App-header').length).toEqual(1);
+    });
 
-// Finding content that the component should render
-it('renders a div with an App-footer class', () => {
-	expect(wrapper.find('.App-footer').length).toEqual(1);
-});
+    // Finding content that the component should render
+    it('renders a div with an App-footer class', () => {
+        expect(wrapper.find('.App-footer').length).toEqual(1);
+    });
 
-// Snapshot testing
-it('matches the snapshot', () => {
-    expect(shallowToJson(wrapper)).toMatchSnapshot();
-});
+    // Snapshot testing
+    it('matches the snapshot', () => {
+        expect(shallowToJson(wrapper)).toMatchSnapshot();
+    });
 
-// Testing a connected component using a mock store
-it('renders without crashing', () => {
-	const mockStore = configureStore();
-    const store = mockStore(INITIAL_STATE);
-    const div = document.createElement('div');
-    ReactDOM.render(<Provider store={store}><App /></Provider>, div);
+    // Testing a connected component using a mock store
+    it('renders without crashing', () => {
+        const middlewares = [reduxThunk];
+        const mockStore = configureStore(middlewares);
+        const store = mockStore(INITIAL_STATE);
+        const div = document.createElement('div');
+        ReactDOM.render(<Provider store={store}><App /></Provider>, div);
+    });
 });
--- src/components/Button.test.js
+++ src/components/Button.test.js
@@ -3,49 +3,50 @@ import ReactDOM from 'react-dom';
 import { shallow } from 'enzyme';
 import { shallowToJson } from 'enzyme-to-json';
 import { Button } from './Button';
-import INITIAL_STATE from '../reducers/initialState';
 
 let wrapper, props;
 
-beforeEach(() => {
-	props = {
-		buttonText: 'hey look at me',
-		clickHandler: () => { /* do nothing */ },
-	},
-	wrapper = shallow(<Button {...props} />);
-});
+describe('Button', () => {
+    beforeEach(() => {
+        props = {
+            buttonText: 'hey look at me',
+            clickHandler: () => { /* do nothing */ },
+        };
+        wrapper = shallow(<Button {...props} />);
+    });
 
-// Shallow rendering the unconnected component
-it('renders only one Button', () => {
-	expect(wrapper.length).toEqual(1);
-});
+    // Shallow rendering the unconnected component
+    it('renders only one Button', () => {
+        expect(wrapper.length).toEqual(1);
+    });
 
-// Finding content that the component should render
-it('renders a button element', () => {
-	expect(wrapper.find('button').length).toEqual(1);
-});
+    // Finding content that the component should render
+    it('renders a button element', () => {
+        expect(wrapper.find('button').length).toEqual(1);
+    });
 
-// Checking on prop the component received
-it('shows the correct button text based on the props', () => {
-	expect(wrapper.find('button').prop('children')).toEqual('hey look at me');
-});
+    // Checking on prop the component received
+    it('shows the correct button text based on the props', () => {
+        expect(wrapper.find('button').prop('children')).toEqual('hey look at me');
+    });
 
-// Snapshot testing
-it('matches the snapshot', () => {
-    expect(shallowToJson(wrapper)).toMatchSnapshot();
-});
+    // Snapshot testing
+    it('matches the snapshot', () => {
+        expect(shallowToJson(wrapper)).toMatchSnapshot();
+    });
 
-// Rendering a component into the DOM without setting up a mock store since it and it's children are not connected
-it('renders without crashing', () => {
-    const div = document.createElement('div');
-    ReactDOM.render(<Button {...props} />, div);
-});
+    // Rendering a component into the DOM without setting up a mock store since it and it's children are not connected
+    it('renders without crashing', () => {
+        const div = document.createElement('div');
+        ReactDOM.render(<Button {...props} />, div);
+    });
 
-// Click events
-it('responds to the click evenet', () => {
-	const spy = jest.spyOn(props, 'clickHandler');
-	wrapper = shallow(<Button {...props} />);
-	wrapper.find('button').simulate('click', {});
-    expect(spy).toHaveBeenCalledTimes(1);
-    spy.mockRestore();
+    // Click events
+    it('responds to the click event', () => {
+        const spy = jest.spyOn(props, 'clickHandler');
+        wrapper = shallow(<Button {...props} />);
+        wrapper.find('button').simulate('click', {});
+        expect(spy).toHaveBeenCalledTimes(1);
+        spy.mockRestore();
+    });
 });
